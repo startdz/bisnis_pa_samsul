2cb859b28c7cbd5e63ea0ba672f7dcdd8d4dad92
Fuad <fuaddzakwan26@gmail.com>
2023-04-29 20:09:40 +0700

    menambah fitur update controller pada yoghurt

--- server/controllers/YoghurtController.js
+++ server/controllers/YoghurtController.js
@@ -2,18 +2,18 @@ import Yoghurt from '../models/Yoghurt.js'
 
 const YoghurtController = {
 
-    createYoghurt: async(req, res, next) => {
+    createYoghurt: async (req, res, next) => {
 
         const img = req.file
 
         if (!img) {
-            res.status(422).json({message: "Tidak ada gambar yang ter-upload!"})
+            res.status(422).json({ message: "Tidak ada gambar yang ter-upload!" })
             return false
         } else if (img.size >= 2000000) {
-            res.status(422).json({message: "Ukuran gambar max 2mb"})
+            res.status(422).json({ message: "Ukuran gambar max 2mb" })
             return false
         } else if (img.mimetype !== 'image/png' || img.mimetype !== 'image/jpg' || img.mimetype !== 'image/jpeg') {
-            res.status(422).json({message: "Format gambar tidak didukung!, Isi data yang benar"})
+            res.status(422).json({ message: "Format gambar tidak didukung!, Isi data yang benar" })
             return false
         }
 
@@ -33,25 +33,106 @@ const YoghurtController = {
             await create.save()
             res.status(201).json(create)
         } catch (error) {
-            res.status(500).json({message: error.message})      
+            res.status(500).json({ message: error.message })
         }
     },
 
-    getYoghurts: async(req, res, next) => {
+    getYoghurts: async (req, res, next) => {
         try {
             const results = await Yoghurt.find()
             res.status(200).json(results)
         } catch (error) {
-            res.status(404).json({message: error.message})
+            res.status(404).json({ message: error.message })
         }
     },
 
-    getYoghurt: async(req, res, next) => {
+    getYoghurt: async (req, res, next) => {
         try {
             const result = await Yoghurt.findById(req.params.id)
             res.status(200).json(result)
         } catch (error) {
-            res.status(404).json({message: error.message})
+            res.status(404).json({ message: error.message })
+        }
+    },
+
+    updateYoghurt: async (req, res, next) => {
+
+        const img = req.file
+
+        const _dataID = await Yoghurt.findById(req.params.id)
+        const { image: { data, originalName, ContentType, createdAt } } = _dataID
+
+        let image = {
+            data: {},
+            originalName: '',
+            ContentType: '',
+            createdAt: {}
+        }
+        let url = ''
+
+        if (!img) {
+            image = { data, originalName, ContentType, createdAt }
+            url = _dataID.url
+        } else {
+            if (img.size >= 2000000) {
+                res.status(422).json({message: "Gambar max 2mb"})
+                return false
+            }
+            if (img.mimetype !== 'image/png' ||img.mimetype !== 'image/jpg' || img.mimetype !== 'image/jpeg') {
+                res.status(422).json({message: "Format gambar tidak didukung!"})
+                return false
+            }
+
+            try {
+                const update = await Yoghurt.updateOne(
+                    _dataID,
+                    {
+                        $set: {
+                            title: req.body.title,
+                            description: req.body.description,
+                            image: {
+                                data: img.encoding,
+                                originalName: img.filename,
+                                ContentType: img.mimetype
+                            },
+                            url: `${req.protocol}://${req.get("host")}/images/${img.filename}`,
+                            price: req.body.price,
+                            stock: req.body.stock
+                        }
+                    }
+                )
+                res.status(200).json(update)
+            } catch (error) {
+                res.status(500).json({message: error.message})
+            }
+        }
+    },
+
+    /**
+     * Method for Debugging, Below :
+     * Posting
+     * Update 
+     * Get
+     * @getDebugYoghurt
+     * @postDebugYoghurt
+     */
+
+    getDebugYoghurt: async (req, res, next) => {
+        try {
+            const _dataID = await Yoghurt.findById(req.params.id)
+            const { image: { data, originalName, ContentType } } = _dataID
+            res.json(originalName)
+        } catch (error) {
+
+        }
+    },
+
+    postDebugYoghurt: async (req, res, next) => {
+        try {
+            const _postDebugImage = req.file
+            console.log(_postDebugImage)
+        } catch (error) {
+
         }
     }
 
--- server/routes/YoghurtRouter.js
+++ server/routes/YoghurtRouter.js
@@ -9,14 +9,14 @@ router.get('/api/product/yoghurt/', YoghurtController.getYoghurts)
 //create
 router.post('/api/product/yoghurt/', Upload.single('image'), YoghurtController.createYoghurt)
 //update
-// router.put('/api/product/yoghurt/:id', Upload.single('image'), updateYoghurt)
+router.put('/api/product/yoghurt/:id', Upload.single('image'), YoghurtController.updateYoghurt)
 // //get
-// router.get('/api/product/yoghurt/:id', getYoghurt)
+router.get('/api/product/yoghurt/:id', YoghurtController.getYoghurt)
 
 // /**
 //  * Debugger
 //  */
-// router.get('/api/product/deyoghurt/:id', getDebugYoghurt)
-// router.post('/api/product/deyoghurt/', Upload.single('image'), postDebugYoghurt)
+router.get('/api/product/deyoghurt/:id', YoghurtController.getDebugYoghurt)
+router.post('/api/product/deyoghurt/', Upload.single('image'), YoghurtController.postDebugYoghurt)
 
 export default router
