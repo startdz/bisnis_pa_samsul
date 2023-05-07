049cd497942befe8a04e0c3d9adb25c657a5fad2
Fuad <fuaddzakwan26@gmail.com>
2023-05-07 12:00:37 +0700

    method kurma done ?

--- server/models/Kurma.js
+++ server/models/Kurma.js
@@ -1,32 +1,33 @@
 import mongoose from "mongoose";
-const {Schema, model} = mongoose
+const { Schema, model } = mongoose;
 
 const kurmaSchema = Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: Text,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    },
-    url: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    stock: {
-        type: Number,
-        required: true
-    },
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    data: Buffer,
+    originalName: String,
+    ContentType: String,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+});
 
-const Kurma = model('kurma', kurmaSchema)
+const Kurma = model("kurma", kurmaSchema);
 export default Kurma;
--- server/routes/KurmaRouter.js
+++ server/routes/KurmaRouter.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import KurmaController from '../controllers/KurmaController.js'
+import Upload from '../middlewares/Images.js'
 
 const router = express.Router()
+
+router.get('/api/product/kurma', KurmaController.listsKurma)
+router.get('/api/product/kurma/:id', KurmaController.getKurma)
+router.post('/api/product/kurma', Upload.single('images'), KurmaController.createKurma)
+router.put('/api/product/kurma/:id', Upload.single('images'), KurmaController.updateKurma)
+router.delete('/api/product/kurma/:id', KurmaController.removeKurma)
 
 export default router
